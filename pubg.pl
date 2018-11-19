e878cdeb34684ebc1144f00fbe027b3848e37aa4
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2018-11-19 14:07:53 +0700

    commit 2

--- pubg.pl
+++ pubg.pl
@@ -26,3 +26,43 @@ medicine(bandage, 15).
 #di random
 
 #beberapa rule dan fakta tambahan
+
+#command
+start() :- print("Welcome to the battlefield!
+You have been chosen as one of the lucky contestants. Be the last man standing
+and you will be remembered as one of the victors.
+Available commands:
+start. -- start the game!
+help. -- show available commands
+quit. -- quit the game
+look. -- look around you
+n. s. e. w. -- move
+map. -- look at the map and detect enemies
+take(Object). -- pick up an object
+drop(Object). -- drop an object
+use(Object). -- use an object
+attack. -- attack enemy that crosses your path
+status. -- show your status
+save(Filename). -- save your game
+load(Filename). -- load previously saved game"), nl,
+
+print("Legends:
+W = weapon
+A = armor
+M = medicine
+O = ammo
+P = player
+E = enemy
+- = accessible
+X = inaccessible").
+
+status :- pemain(X,Y,Z), print("Health : ", X), nl.
+status :- pemain(X,Y,Z), print("Armor : ", Y), nl.
+status :- pemain(X,Y,Z), print("Inventory : ", Z), nl.
+
+generatepeta(List, N, [Head | Tail]) :-
+	append(Head, Other, List),
+	length(Start,N).
+	gen(Other,N, Tail).
+
+map :- generatepeta(listpeta, 10, peta), print(peta).
